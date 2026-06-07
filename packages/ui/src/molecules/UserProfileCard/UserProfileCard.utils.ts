dbca64399db10c6410254b11a0a1987b5645ffe1
Nguyen Quang Huy <qnguyenhuy1999@gmail.com>
2026-06-07 15:03:57 +0700

    chore: clean up components not use

--- packages/ui/src/molecules/UserProfileCard/UserProfileCard.utils.ts
+++
@@ -1,9 +0,0 @@
-export function getInitials(name: string | null): string {
-  if (!name) return '?'
-  return name
-    .split(' ')
-    .map((part) => part[0])
-    .join('')
-    .toUpperCase()
-    .slice(0, 2)
-}
